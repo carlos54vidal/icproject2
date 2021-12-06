9a9db787b64e487a9b59ecff8e38db3b6f214b00
zpdomingues16 <jdomingues@ua.pt>
2021-12-06 17:23:45 +0000

    golomb

---
+++ ex1/Golomb.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+
+
+using namespace std;
+
+class Golomb{
+ 
+    public:
+    int m; 
+    
+
+
+    void encode(int n, int m){
+       int q;
+       int r;
+       
+       q = n/m; //código unário
+       r = n%m; //código binário
+
+
+    }
+
+    void decode(){
+
+
+    }
+
+
+
+}
