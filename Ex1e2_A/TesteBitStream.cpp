ae9ef67ddf06ad5bc76dfda9fbcf60ac0d356e0e
carlos54vidal <94443213+carlos54vidal@users.noreply.github.com>
2021-12-08 22:06:17 +0000

    Update

--- Ex1e2_A/TesteBitStream.cpp
+++ Ex1e2_A/TesteBitStream.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <math.h>
+#include <sstream>
 #include "BitStream.h"
 
 
 
 
 
-int main ()
+int main (int argc, char* argv[])
 {
+///  argv[1]=Ajuda (/?), ou caminho e nome do ficheiro original
+///  argv[2]=caminho e nome do ficheiro novo
+///  argv[3]=indicação da flag -r ou -w, -r para ler ou -w para escrever
+///  argv[4]=indicação da flag -b, para indicar o n.º de bits a ler e escrever
+///  argv[5]=valor de [-b size], para indicar o n.º de bits a ler e escrever
 
-///bs ("novo_ficheiro.txt", "w");
+
+
+        if (argc<=2 || argv[1]=="/?")	{	// indicação da sintaxe de commando
+		cout << "Usage: ./program_name ./original_file_name.wav ./copy_file_name.wav -r or -w [-b size] \n\n options:\n\t -b size\t Number of bits to read or write (all if not specified).\n" << endl; // \a toca um som de alerta (byte 0x07 in ASCII encoding)
+	}
+
+
+    if (argc==5){
+        string option=argv[4];
+        if (option=="-b")	{	     // indicação da sintaxe de commando
+		cout << "The number of bits to read or write after the argument \"-b\", is missing\n\n options:\n\t -b size\t Number of bits to read or write (all if not specified).\n" << endl; // \a toca um som de alerta (byte 0x07 in ASCII encoding)
+
+		}}
+
+
+    if (argc==6){
+
+        long numbits=atoi(argv[5]);
+        Read tb();
+        tb.total_nbits=ceil(numbits);
+        cout << "argc=6" << endl;
+        cout << numbits << endl;
+
+    }
+
+
+    else {
+
+
+
+    if (argc==4){
+
 
     BitStream bs("teste.txt", "w");
 
@@ -31,5 +70,12 @@ int main ()
     bs.Write (1);
 
 //    bs.Write.ofs.close()
+
+
+    }
+
+
+
+    }
 return 0;
 }
